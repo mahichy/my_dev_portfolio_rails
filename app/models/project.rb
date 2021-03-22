09ac405f778094f2fcac96a526ac8f324faeb117
mahi <mohiddinoshim@yahoo.com>
2021-03-22 22:20:17 +0600

    add carrierwave gem

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,19 +3,19 @@ class Project < ApplicationRecord
 	# if Rails.env.development?
 	# 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
 	# else
-		has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" },
-							:storage => :dropbox,
-						    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-						    :path => ":style/:id_:filename"
+	# 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" },
+	# 						:storage => :dropbox,
+	# 					    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+	# 					    :path => ":style/:id_:filename"
+	# # end
+
+ #  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+	# validates :name, presence: true
+	# validates :content, presence: true
+
+	# def shortcontent
+	# 	content.length > 130? content[0..130] + "..." : content
 	# end
-
-  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
-	validates :name, presence: true
-	validates :content, presence: true
-
-	def shortcontent
-		content.length > 130? content[0..130] + "..." : content
-	end
 
 end
