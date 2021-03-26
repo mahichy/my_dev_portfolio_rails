9fd16d0785a58f0ca3c3e3aa2fc91cf95fdf743e
mahi <mohiddinoshim@yahoo.com>
2021-03-27 00:45:31 +0600

    Remove image migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_162358) do
+ActiveRecord::Schema.define(version: 2021_03_26_183933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,10 +57,6 @@ ActiveRecord::Schema.define(version: 2021_03_22_162358) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.bigint "image_file_size"
-    t.datetime "image_updated_at"
     t.string "url"
     t.text "technology"
     t.text "app_how"
